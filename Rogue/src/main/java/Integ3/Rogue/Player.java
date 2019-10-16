c78bf450f7eff4b018e6761ebb10ab00a6f73c03
Thing Thinge <thing@something.com>
2019-10-16 23:17:41 +0200

    added example tests

---
+++ Rogue/src/main/java/Integ3/Rogue/Player.java
@@ -0,0 +1,6 @@
+package Integ3.Rogue;
+
+public class Player {
+
+
+}
---
+++ Rogue/src/test/java/Integ3/Rogue/PlayerMovementTest.java
@@ -0,0 +1,35 @@
+package Integ3.Rogue;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class PlayerMovementTest {
+
+
+
+    /*
+
+    @Test
+    public void playerMoveUpTest(){
+        assertEquals((Player.getY()+1),Player.getY());
+    }
+
+    @Test
+    public void playerMoveDownTest(){
+        assertEquals((Player.getY()-1),Player.getY());
+    }
+
+    @Test
+    public void playerMoveLeftTest(){
+        assertEquals((Player.getX()-1),Player.getX());
+    }
+    @Test
+    public void playerMoveRightTest(){
+        assertEquals((Player.getX()+1),Player.getX());
+    }
+*/
+
+
+
+
+}
