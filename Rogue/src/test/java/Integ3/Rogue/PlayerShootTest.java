81c95b4dc9f62c052c1737346eef4e4d6f3e6c55
Empa Emanuel <empaemanuel@gmail.com>
2019-10-23 19:30:23 +0200

    New branch for shoot functionality

---
+++ Rogue/src/test/java/Integ3/Rogue/PlayerShootTest.java
@@ -0,0 +1,15 @@
+package Integ3.Rogue;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PlayerShootTest {
+
+    @Test
+    public void centerHit(){
+        assertTrue(true);
+    }
+
+
+
+}
