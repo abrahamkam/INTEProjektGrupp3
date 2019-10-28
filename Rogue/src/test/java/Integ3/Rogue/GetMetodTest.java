40325c2934a257b9e85ca68e63403fa4a02adcab
Ferdousy <panna123@yahoo.com>
2019-10-28 21:47:49 +0100

    Add Test

--- Rogue/src/test/java/Integ3/Rogue/GetMetodTest.java
+++ Rogue/src/test/java/Integ3/Rogue/GetMetodTest.java
@@ -5,13 +5,16 @@ import static org.junit.Assert.*;
 import org.junit.Assert;
 import org.junit.Test;
 
+import Integ3.Rogue.Player.direction;
+
 
 public class GetMetodTest {
 	Player player = new Player(10,20);
 	Enemy enemy = new Enemy (5, 10, player);
 	GameScreen gameScreen = new GameScreen();	
 	Score score= new Score(100);
-	
+	Actor actor = new Actor(5,5, 60);
+	Player plr = new Player(10,5);
 	@Test
 	
 	public void getXTest() {
@@ -44,7 +47,18 @@ public class GetMetodTest {
 	    AssertEquals(300,width);
 	  	
 	}
-
+	@Test 
+	public void getLengthTest() {
+		int length = actor.getLength();
+		AssertEquals(60,length);
+	}
+	
+	@Test
+	public void getScreen() {
+		int height = gameScreen.getGameHeight();
+		AssertEquals(400,height);
+		
+	}
 
 	private void AssertEquals(int i, int a) {
 		
