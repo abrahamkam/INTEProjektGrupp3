24552814e92df5dd3018fd8ba54ac5e3b17108ed
Ferdousy <panna123@yahoo.com>
2019-10-30 15:07:40 +0100

    Add getPlayerTest and getEnemyTest

--- Rogue/src/test/java/Integ3/Rogue/GetMetodTest.java
+++ Rogue/src/test/java/Integ3/Rogue/GetMetodTest.java
@@ -9,60 +9,67 @@ import Integ3.Rogue.Player.direction;
 
 
 public class GetMetodTest {
-	Player player = new Player(10,20);
-	Enemy enemy = new Enemy (5, 10, player);
-	GameScreen gameScreen = new GameScreen();	
+	
+	int playerx =5,playery=7,enemyx=6,enemyy=8;
+	GameScreen gameScreen = new GameScreen(playerx,playery,enemyx,enemyy);	
 	Score score= new Score(100);
-	Actor actor = new Actor(5,5, 60);
-	Player plr = new Player(10,5);
+	Actor actor = new Actor(10,20,60);
+	
+	
 	@Test
 	
 	public void getXTest() {
-	int a = player.getX();
-	AssertEquals(10,a);
+	int a = actor.getX();
+	assertEquals(10,a);
 		
 }
-	@Test
-	public void getYTest() {
-		int b = enemy.getY();
-		AssertEquals(10,b); 
-	}
 	
 	@Test
-	public void getWidthTest() {
+	public void getYTest() {
+		int b = actor.getY();
+		assertEquals(20,b); 
+	}
+	
+	@Test 
+	public void getLengthTest() {
+		int length = actor.getLength();
+		assertEquals(60,length);
+	}
+	
+	
+	@Test
+	public void getGameWidthTest() {
 	    int width =gameScreen.getGameWidth();
-	    AssertEquals(200,width);
+	    assertEquals(200,width);
+	  	
+	}
+	@Test
+	public void getGameHeightTest() {
+	    int width =gameScreen.getGameHeight();
+	    assertEquals(300,width);
 	  	
 	}
 	
+	@Test
+	public void getPlayer() {
+		int x = gameScreen.getPlayer().getX();
+		int y = gameScreen.getPlayer().getY();
+		assertEquals(playerx,x);
+		assertEquals(playery,y);
+	}
+
+		
+	@Test
+	public void getEnemy() {
+		int x = gameScreen.getEnemy().getX();
+		int y = gameScreen.getEnemy().getY();
+		assertEquals(enemyx,x);
+		assertEquals(enemyy,y);
+	}
 	@Test
 	public void getScoreTest() {
 		int s = score.getPlayerScore();
 		assertEquals(100,s);
-	}
-	
-	@Test
-	public void getHeightTest() {
-	    int width =gameScreen.getGameHeight();
-	    AssertEquals(300,width);
-	  	
-	}
-	@Test 
-	public void getLengthTest() {
-		int length = actor.getLength();
-		AssertEquals(60,length);
-	}
-	
-	@Test
-	public void getScreen() {
-		int height = gameScreen.getGameHeight();
-		AssertEquals(400,height);
-		
-	}
-
-	private void AssertEquals(int i, int a) {
-		
-		
 	}
 
 
